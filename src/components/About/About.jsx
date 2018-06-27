eebfebe3b7f510ccaa9d33c1d1108929ab62ad20
Matt Webb <matt.webb@connectedfi.com>
2018-06-27 08:56:03 -0600

    adding about DenverScript info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,13 +11,27 @@ class About extends Component {
       <div className="about-container md-grid mobile-fix">
         <Card className="md-grid md-cell--8">
           <div className="about-wrapper">
-            <img
-              src={config.userAvatar}
-              className="about-img"
-              alt={config.userName}
-            />
             <CardText>
-              <p className="about-text md-body-1">{config.userDescription}</p>
+              <p className="about-text md-body-1">
+                DenverScript is a community of developers focused on JavaScript
+                in downtown Denver. We will be discussing best practices,
+                features coming to ECMAScript, application development, what's
+                going on in the JS community, and more. If you have any interest
+                in upping your JavaScript knowledge, then please come check us
+                out.
+              </p>
+              <p className="about-text md-body-1">
+                Follow our Twitter account at @DenverScript
+                (https://twitter.com/denverscript) for JavaScript tweets and
+                Meetup announcements, and join us at #denverscript on Freenode
+                to chat!
+              </p>
+              <p className="about-text md-body-1">
+                We care about providing a safe harassment-free community, so
+                read our Code of Conduct
+                (http://www.meetup.com/DenverScript/pages/Code_of_Conduct/) and
+                know who to contact if there are issues.
+              </p>
             </CardText>
             <UserLinks labeled config={config} />
           </div>
